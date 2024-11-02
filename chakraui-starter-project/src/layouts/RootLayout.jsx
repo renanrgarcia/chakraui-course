38cdefe47fe8cd36f24da3f56c8bceef460b71fc
Renan Garcia <renanrgarcia@gmail.com>
2024-11-02 06:58:09 -0300

    lesson-5

--- chakraui-starter-project/src/layouts/RootLayout.jsx
+++ chakraui-starter-project/src/layouts/RootLayout.jsx
@@ -8,15 +8,16 @@ export default function RootLayout() {
       <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
         <GridItem
           as="aside"
-          colSpan="1"
+          colSpan={{ base: 6, lg: 2, xl: 1 }}
           bg="purple.400"
-          minH="100vh"
+          minH={{ lg: "100vh" }}
+          p={{ base: "20px", lg: "30px" }}
         >
           <span>sidebar</span>
         </GridItem>
         <GridItem
           as="main"
-          colSpan="5"
+          colSpan={{ base: 6, lg: 4, xl: 5 }}
           p="40px"
         >
           <Navbar />
--- chakraui-starter-project/src/pages/Dashboard.jsx
+++ chakraui-starter-project/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
-import { Box, LinkBox, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
 
   return (
     <SimpleGrid p={10} spacing={10} minChildWidth={250}>
-      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid">
+        <Text color={{ base: "pink", md: "blue", lg: "green" }}>Hello</Text>
+      </Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
