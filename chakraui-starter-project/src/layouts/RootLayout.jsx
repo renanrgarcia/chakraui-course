abe7b19abf93e29bb13c06f2e5ad4e2bee50c8c6
Renan Garcia <renanrgarcia@gmail.com>
2024-11-01 21:05:52 -0300

    lesson-4

--- chakraui-starter-project/src/layouts/RootLayout.jsx
+++ chakraui-starter-project/src/layouts/RootLayout.jsx
@@ -1,11 +1,29 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import { Grid, GridItem } from "@chakra-ui/react"
 
 export default function RootLayout() {
   return (
     <div>
-      <Navbar />
-      <Outlet />
+      <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+        <GridItem
+          as="aside"
+          colSpan="1"
+          bg="purple.400"
+          minH="100vh"
+        >
+          <span>sidebar</span>
+        </GridItem>
+        <GridItem
+          as="main"
+          colSpan="5"
+          p="40px"
+        >
+          <Navbar />
+          <Outlet />
+        </GridItem>
+      </Grid>
+      
     </div>
   )
 }
--- chakraui-starter-project/src/pages/Dashboard.jsx
+++ chakraui-starter-project/src/pages/Dashboard.jsx
@@ -1,37 +1,23 @@
-import { Heading, Text, Container, Box } from "@chakra-ui/react";
+import { Box, LinkBox, SimpleGrid } from "@chakra-ui/react";
 
 export default function Dashboard() {
 
-  const boxStyles = {
-    p: "10px",
-    m: "10px",
-    bg: "purple.400",
-    color: "white",
-    textAlign: "center",
-    filter: "blur(2px)",
-    ':hover': {
-      color: "black",
-      bg: "blue.200",
-    }
-  }
-
   return (
-    <Container as="section" maxWidth="4xl" py="20px">
-      <Heading my="30px" p="10px">Hello world!</Heading>
-      <Text marginLeft="30px">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, excepturi!
-      </Text>
-      <Text ml="30px" color="blue.300" fontWeight="bold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, excepturi!
-      </Text>
-      
-      <Box my="30px" p="20px" bg="orange">
-        <Text color="white">This is a Box</Text>
-      </Box>
+    <SimpleGrid p={10} spacing={10} minChildWidth={250}>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
 
-      <Box sx={boxStyles}>
-        Hello, Ninjas!
-      </Box>
-    </Container>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+    </SimpleGrid>
   )
 }
