0d81ab00a750395aa4a75b578e66c29cc2ccd6e2
Renan Garcia <renanrgarcia@gmail.com>
2024-11-08 08:15:17 -0300

    lesson-10

--- chakraui-starter-project/src/components/Navbar.jsx
+++ chakraui-starter-project/src/components/Navbar.jsx
@@ -1,24 +1,39 @@
-import { Box, Button, Flex, Heading, HStack, Spacer, Text } from "@chakra-ui/react";
+import { UnlockIcon } from "@chakra-ui/icons"
+import { 
+  Flex, 
+  Heading, 
+  Box, 
+  Text, 
+  Button, 
+  Spacer, 
+  HStack, 
+  useToast 
+} from "@chakra-ui/react"
 
 export default function Navbar() {
+  const toast = useToast()
+
   return (
-    <Flex as="nav" p="10px" alignItems="center" mb="40px">
-      <Heading as="h1">Dojo Tasks</Heading>
+    <Flex as="nav" p="10px" mb="60px" alignItems="center">
+      <Heading as="h1" fontSize="1.5em">Dojo Tasks</Heading>
       <Spacer />
 
-      <HStack spacing="20px">
-        <Box bg="gray.200" p="10px">M</Box>
+      <HStack spacing="20px"> 
+        <Box bg="gray.200" p="10px 15px" borderRadius="50%">M</Box>
         <Text>mario@netninja.dev</Text>
-        <Button>Logout</Button>
+        <Button 
+          colorScheme="purple"
+          onClick={() => toast({
+            title: 'Logged out.',
+            description: "Successfully logged out",
+            duration: 10000,
+            isClosable: true,
+            position: 'top',
+            status: 'success',
+            icon: <UnlockIcon />,
+          })}
+        >Logout</Button>
       </HStack>
     </Flex>
-
-    // <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
-    //   <Box w="150px" h="50px" bg="red">1</Box>
-    //   <Box w="150px" h="50px" bg="blue">2</Box>
-    //   <Box w="150px" h="50px" flexGrow="1" bg="green">3</Box>
-    //   <Box w="150px" h="50px" flexGrow="2" bg="yellow">4</Box>
-    // </Flex>
   )
 }
-
