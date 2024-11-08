a1d55cdda0402b7fb94fca45773e3070d88e9581
Renan Garcia <renanrgarcia@gmail.com>
2024-11-08 11:24:41 -0300

    lesson-12

--- chakraui-starter-project/src/layouts/RootLayout.jsx
+++ chakraui-starter-project/src/layouts/RootLayout.jsx
@@ -1,31 +1,33 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../components/Navbar"
 import { Grid, GridItem } from "@chakra-ui/react"
-import SideBar from "../components/SideBar"
+import { Outlet } from "react-router-dom"
+
+// components
+import Navbar from "../components/Navbar"
+import Sidebar from "../components/Sidebar"
 
 export default function RootLayout() {
   return (
-    <div>
-      <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
-        <GridItem
-          as="aside"
-          colSpan={{ base: 6, lg: 2, xl: 1 }}
-          bg="purple.400"
-          minH={{ lg: "100vh" }}
-          p={{ base: "20px", lg: "30px" }}
-        >
-          <SideBar />
-        </GridItem>
-        <GridItem
-          as="main"
-          colSpan={{ base: 6, lg: 4, xl: 5 }}
-          p="40px"
-        >
-          <Navbar />
-          <Outlet />
-        </GridItem>
-      </Grid>
-      
-    </div>
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+      {/* sidebar */}
+      <GridItem
+        as="aside"
+        colSpan={{ base: 6, lg: 2, xl: 1 }} 
+        bg="purple.400"
+        minHeight={{ lg: '100vh' }}
+        p={{ base: '20px', lg: '30px' }}
+      >
+        <Sidebar />
+      </GridItem>
+
+      {/* main content & navbar */}
+      <GridItem
+        as="main"
+        colSpan={{ base: 6, lg: 4, xl: 5 }} 
+        p="40px"
+      >
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
   )
 }
